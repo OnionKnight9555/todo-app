863dff676795c7074eb710a0d507e715be6d26bf
Richard Rios <richardrios@Richards-MacBook-Air-2.local>
2026-04-02 08:14:04 -0700

    added a loop feature, as well as an option to save tasks into a list

--- todo.py
+++ todo.py
@@ -1,15 +1,22 @@
-print("--- My To-Do List ---")
-print("1. View Tasks")
-print("2. Add Task")
-print("3. Quit")
+tasks = []
 
-choice = input("Choose an option: ")
+while True:
+    print("--- My To-Do List ---")
+    print("1. View Tasks")
+    print("2. Add Task")
+    print("3. Quit")
 
-if choice == "1":
-    print("You selected: View Tasks")
-elif choice == "2":
-    print("You selected: Add Task")    
-elif choice == "3":
-    print("Goodbye!")
-else:
-    print("Hey that wasn't an option! Try again")
+    choice = input("Choose an option [1-3]: ")
+
+    if choice == "1":
+        print("You selected: View Tasks")
+    elif choice == "2":
+        task = input("Enter your task: ")
+        tasks.append(task)
+        print("Task added.")    
+    elif choice == "3":
+        print("Goodbye!")
+    elif choice == "hello":
+        print("Nice to meet you, but please choose a number now!")
+    else:
+        print("Hey that wasn't an option! Try again")
