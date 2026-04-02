8a71da9b3511ad588d56a463b4633582c140d31a
Richard Rios <richardrios@Richards-MacBook-Air-2.local>
2026-04-01 22:42:47 -0700

    added a basic menu plus user input line. need to add a cleaner response when user selects an option

--- todo.py
+++ todo.py
@@ -1,1 +1,7 @@
-print("Hello, World!")
+print("--- My To-Do List ---")
+print("1. View Tasks")
+print("2. Add Task")
+print("3. Quit")
+
+choice = input("Choose an option: ")
+print("You chose:", choice)
